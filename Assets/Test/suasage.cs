9b47469f0cf885945453a5731dbd366aafff95ed
BalazsKozma <kozma.balazs1992@gmail.com>
2017-03-08 23:23:12 +0100

    Added script

---
+++ Assets/Test/suasage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class suasage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("haha");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
